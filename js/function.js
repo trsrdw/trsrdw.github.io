f4bc415273baf7c25af545c3a300e1f0da22d5c3
Tiara S Dewi <trsrdw@gmail.com>
2023-03-11 16:27:56 +0700

    Initial commit

---
+++ js/function.js
@@ -0,0 +1,19 @@
+function myFunction(imgs) {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var modalImg = document.getElementById("img01");
+
+    modal.style.display = "block";
+    modalImg.src = imgs.src;
+
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+}
